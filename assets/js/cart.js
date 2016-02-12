d3725e8deb7e88c14d839f296a3563c9f4bf2cee
Monica Ricks <monicaricks@gmail.com>
2016-02-12 01:57:55 +0000

    Basic requirements of amazoon jquery project

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -23,6 +23,71 @@ $(document).ready(function(){
       }
     ];
 
-    // Get started here!
+    var updateView = function(){
+      //hide/show the .empty h3 tags based on the 
+      var productsInCart = $('.cart .product');
+      var productsSaved = $('.cart .product');
+      if(productsInCart.length === 0){
+        $('.cart .empty').show();
+        $('.cart-total').hide();
+      }else{
+        $('.cart .empty').hide();
+         $('.cart-total').show();
+      }
+      if(productsSaved.length === 0){
+        $('.saved .empty').show();
+         //$('.cart-total').hide()]=
+      }else{
+        $('.saved .empty').hide();
+      }
+
+
+      //update the cart's total
+      var totalPrice = 0;
+      productsInCart.each(function()){
+        totalPrice += Number($(this).data('price'));
+      }
+    $('.cart-total .red').text("$" + totalPrice.toFixed(2));
+
+    };
+
+    var productTemplate = $('template#product').html();
+  
+    for(var i = 0; i < products.length; i++){
+
+      var newProduct = $(productTemplate);
+
+      $('h2', newProduct).text(products[i].name);
+
+      var newManufacturer = $('<span/>').addClass('manufacturer');
+      newManufacturer.text("by " + products[i].manufacturer);
+      $('h2',newProduct).append(newManufacturer);
+
+      $('img', newProduct).attr("src", products[i].imageSrc);
+      $('h3', newProduct).text(products[i].description);
+      $('.price, .mobile-price', newProduct).text("$" + products[i].price.toFixed(2));
+      //setting the price as key
+      $(newProduct).data('price', products[i].price);
+      //getting the price 
+      //$(newProduct).data('price');
+
+      $('.cart').append(newProduct);
+    }
+    updateView();
+
+
+    $('.move').on('click', function(){
+      var thisClass = $(this).parents().eq(4).attr('class');
+        if(thisClass === 'cart'){
+          $(this).text("Add to cart");
+          $(this).parents().eq(3).prependTo('.saved');
+        }else if(thisClass === 'saved'){
+          $(this).text("Save for later");
+          $(this).parents().eq(3).prependTo('.cart');
+      }
+      updateView();
+      });
+
+
 
   });
--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-  // Get started here!
+      $('.order-form').on('submit', function(event){
+        event.preventDefault();
+        $('.error-message').hide();
+        $('.has-error').removeClass('has-error');
+        var requiredInForm = $('input[required]','.order-form');
+        console.log(requiredInForm);
+
+        var isValid = true;
+        $(requiredInForm).each(function(){
+          if($(this).val().length === 0){
+            console.log($(this).val());
+            $(this).parent().addClass('has-error');
+            $('.error-message').show();
+            isValid = false;
+          }
+        })
+        if(isValid){
+          alert("Congrats on filling out this form!");
+        }
+      });
 });
